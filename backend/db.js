6b28a91a73119582b2308f298e6fc32fb808bd3b
mohaditto <164267114+mohaditto@users.noreply.github.com>
2025-11-19 19:53:07 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || "localhost",
-  user: process.env.MYSQLUSER || "root",
-  port: Number(process.env.MYSQLPORT || 3306),
-  password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "x&y",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
   connectionLimit: 10,
 });
